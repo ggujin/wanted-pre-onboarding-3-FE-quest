f56bfbaf72801ff86eee9f2ae52f1184c5ef19ef
ggujin <yang97816@gmail.com>
2023-03-01 17:36:33 +0900

    feat: add Header

---
+++ src/Layout/Header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <h2>Wanted Pre-onboarding course</h2>
+    </header>
+  );
+}
