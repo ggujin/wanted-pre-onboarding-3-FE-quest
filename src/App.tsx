6807af228ab98f5d32656b8403bc1345bc1a5bab
ggujin <yang97816@gmail.com>
2023-03-05 18:37:50 +0900

    feat: implement router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import { PageA, PageB, PageC } from "./pages/Pages";
+
 import "./styles/global.scss";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<PageA />} />
+      <Route path="/b" element={<PageB />} />
+      <Route path="/c" element={<PageC />} />
+    </Routes>
+  );
 }
 
 export default App;
